c6c3630f00de1a8e94a02190b15a0ed393b5fe33
Marco Lüthy <marco@luethy.net>
2016-12-20 19:57:54 -0800

    got it to deploy correctly

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Lambda-based micro-service to save a given URL as a JPEG, PNG, PDF, etc.
 
 # TODO
 
-- [ ] use S3 object ACLs to and sign access token for accessing object instead of relying simply on object key obfuscation
+- [ ] use S3 object ACLs to and sign access token for accessing object instead of relying simply on object key obfuscation http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
 - [ ] set S3 object expiration if set to non-zero value in config.json
 - [ ] fix up support for CORS
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path')
+const targz = require('tar.gz')
+const webpack = require('webpack')
+
+function ExtractTarballPlugin (archive, to) {
+  return {
+    apply: (compiler) => {
+      compiler.plugin('emit', (compilation, callback) => {
+        targz().extract(path.resolve(archive), path.resolve(to), (error) => {
+          if (error) {
+            return console.error('Unable to extract archive ', archive, to, error.stack)
+          }
+
+          return callback()
+        })
+      })
+    },
+  }
+}
+
+module.exports = {
+  entry: './src/lambda',
+  target: 'node',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        include: __dirname,
+        exclude: /node_modules/,
+      },
+      { test: /\.json$/, loader: 'json-loader' },
+    ],
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: '.webpack',
+    filename: 'lambda.js', // this should match the first part of function handler in serverless.yml
+  },
+  externals: ['sharp', 'phantomjs', 'aws-sdk'],
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({ minimize: true, sourceMap: false, warnings: false }),
+    //new ExtractTarballPlugin('lib/sharp-0.16.1-linux-x64.tar.gz', '.webpack/'),
+    //new ExtractTarballPlugin('lib/phantomjs-XXXX-linux-x64.tar.gz', '.webpack/'),
+  ],
+}
